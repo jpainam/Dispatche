ed5ff82ff1db159aba0dbe1aebd14a0671b4c893
Jean-Paul Ainam <jpainam@gmail.com>
2024-05-26 13:07:00 -0400

    chore: Update TabLayout component with new tab and settings options

---
+++ app/(tabs)/settings/index.tsx
@@ -0,0 +1,82 @@
+import Colors from "@/constants/Colors";
+import { FlatList, ScrollView, Text, View } from "react-native";
+import { defaultStyles } from "@/constants/Styles";
+import BoxedIcon from "@/components/BoxedIcon";
+import { devices, items, support } from "../../../configs/settings";
+import { Ionicons } from "@expo/vector-icons";
+
+const Page = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: Colors.background }}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={{ paddingBottom: 40 }}
+      >
+        <View style={defaultStyles.block}>
+          <FlatList
+            data={devices}
+            scrollEnabled={false}
+            ItemSeparatorComponent={() => (
+              <View style={defaultStyles.separator} />
+            )}
+            renderItem={({ item }) => (
+              <ListItem
+                icon={item.icon}
+                backgroundColor={item.backgroundColor}
+                name={item.name}
+              />
+            )}
+          />
+        </View>
+        <View style={defaultStyles.block}>
+          <FlatList
+            data={items}
+            scrollEnabled={false}
+            ItemSeparatorComponent={() => (
+              <View style={defaultStyles.separator} />
+            )}
+            renderItem={({ item }) => (
+              <ListItem
+                icon={item.icon}
+                backgroundColor={item.backgroundColor}
+                name={item.name}
+              />
+            )}
+          />
+        </View>
+        <View style={defaultStyles.block}>
+          <FlatList
+            data={support}
+            scrollEnabled={false}
+            ItemSeparatorComponent={() => (
+              <View style={defaultStyles.separator} />
+            )}
+            renderItem={({ item }) => (
+              <ListItem
+                icon={item.icon}
+                backgroundColor={item.backgroundColor}
+                name={item.name}
+              />
+            )}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+type ListItemProps = {
+  icon: string;
+  backgroundColor: string;
+  name: string;
+};
+const ListItem = ({ name, backgroundColor, icon }: ListItemProps) => {
+  return (
+    <View style={defaultStyles.item}>
+      <BoxedIcon name={icon} backgroundColor={backgroundColor} />
+      <Text style={{ fontSize: 18, flex: 1 }}>{name}</Text>
+      <Ionicons name="chevron-forward" size={20} color={Colors.gray} />
+    </View>
+  );
+};
+export default Page;
